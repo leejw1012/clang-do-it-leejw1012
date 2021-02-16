197211cacf65dece7932dd7303d3ce1f0345b48e
leejw1012 <45277719+leejw1012@users.noreply.github.com>
2021-02-16 16:57:57 +0900

    Add files via upload

---
+++ chap05-functions.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+/* 함수선언
+*/
+
+void print();
+void printx(int x);
+
+void main(void) {
+	printf("main function!\n");
+	print(); //함수 호출
+	printx(3);
+}
+
+
+/*
+* 함수정의:본체, 내용이 기술
+*/
+
+void print() {
+	printf("print function!\n");
+}
+
+void printx(int x) {
+	printf("printx function%d!\n", x);
+}
